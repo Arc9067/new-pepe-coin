a8aa8e5e2a38b040810ac76cd898c1e236ab850a
Arc <atandarokib@gmail.com>
2023-06-28 11:31:47 +0100

    up

--- src/Components/Mics.jsx
+++ src/Components/Mics.jsx
@@ -10,14 +10,14 @@ const Mics = () => {
       <div className="text-white text-opacity-20 text-[128px] font-memeFont absolute -bottom-3 righleft-0 md:right-0 tracking-widest">
         I
       </div>
-      <div className="container grid lg:grid-cols-2 justify-between items-center gap-14">
+      <div className="container grid l justify-center text-center items-center gap-14">
         <article className="flex flex-col gap-5">
           <h1 className="font-anakeimDisplaySsiRegular text-5xl uppercase md:text-7xl">
-            npepe Tekonomics
+            Tekonomics
           </h1>
           <h2 className="text-2xl">Total Supply: 1, 000, 000, 000</h2>
 
-          <div className="mt-10 flex flex-col gap-3">
+          <div className="mt-10 flex flex-col gap-3 items-center">
             <article className="flex gap-5 items-center capitalize">
               <span className="w-10 h-[13px] relative bg-gradient-to-r from-lime-400 to-green-300 rounded-lg"></span>
               <h3 className="text-xl">0% buy tax</h3>
